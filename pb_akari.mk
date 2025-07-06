38ed01a63f8c45c2b4ad23a7d5e784c18a12516f
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-07-06 17:09:04 +0800

    Update pb_akari.mk

--- pb_akari.mk
+++ pb_akari.mk
@@ -15,11 +15,11 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from device
-$(call inherit-product, device/sony/akari/device.mk) -- path to main device makefile
+$(call inherit-product, device/sony/akari/device.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
